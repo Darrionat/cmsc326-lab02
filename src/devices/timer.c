9ace615b74826a3cb0bd37e10d8ec59fba259475
Nafis <nf5009@bard.edu>
2023-03-14 20:33:15 -0400

    fix alarm multiple

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -101,14 +101,14 @@ timer_sleep (int64_t ticks)
 
   struct thread *t = thread_current();
   // If the semaphore exists it must be equal to 0 because thread is running
-  struct semaphore sema;
+  struct semaphore *sema = (struct semaphore*)malloc(sizeof(struct semaphore));
   if (t->binSema == NULL){
-    sema_init(&sema, 0);
+    sema_init(sema, 0);
     // Set the thread semaphore to this semaphore
-    t->binSema = &sema;
+    t->binSema = sema;
   }
   else{
-    sema = *(t->binSema);
+    sema = (t->binSema);
   }
   // Set the wakeup time for this thread
   t->wakeup_time = ticks + timer_ticks();
@@ -117,7 +117,7 @@ timer_sleep (int64_t ticks)
   
   // list_push_back(&sleeping_threads, &current_thread());
   // Go to sleep
-  sema_down(&sema);
+  sema_down(sema);
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
